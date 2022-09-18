bc363db04a2f980af702d1a7b243004c28cf7bae
Cameron Kramr <cameronkramr@gmail.com>
2022-09-18 03:40:39 -0500

    added the optimization function from scipy to get an optimal answer

--- part_2/ckk5_optimizer.py
+++ part_2/ckk5_optimizer.py
@@ -1,4 +1,5 @@
 import sys
+import os
 import numpy as np
 from scipy.optimize import minimize
 import ckk5_prelab as pb
@@ -13,19 +14,40 @@ def write_sweep_params(filename, param_name, sweep_params):
 		file_obj.write(f" {key}")
 	file_obj.write('\n')
 	
-	print(keys)
+	#print(keys)
 	for param_iter in range(len(sweep_params[keys[0]])):
 		for key in keys:
 			file_obj.write(f" {sweep_params[key][param_iter]}")
 		file_obj.write('\n')
 	file_obj.write(".ENDDATA")
 	file_obj.close()
+
+def Optimize_Function(x, *args):
+	target_spice = args[0]
+	param_file = args[1]
+	param_name = args[2]
+	vector_names = args[3]
+	target_name = args[4]
+	output_file = args[5]
 	
+	#convert the parameters into a dictionary form
+	params = {}
+	for iter, item in enumerate(x):
+		params[vector_names[iter]] = [item]
+	
+	write_sweep_params(param_file, param_name, params)
+	os.system(f"echo {target_spice}")
+	data = pb.import_data(output_file)
+	return float(data[target_name][0])
+
 if __name__ == "__main__":
-	print("Hello world")
-	test_params = {}
-	test_params["len1"] = [i for i in range(10)]
-	test_params["len2"] = [i*i for i in range(10)]
-	test_params["len3"] = [i*2 for i in range(10)]
+	arguments = ("ckk5_critical_path.sp",
+					"sweep_params.sp",
+					"sweep_params",
+					["size_stage_1", "size_stage_2", "size_stage_3"],
+					"delay",
+					"ckk5_critical_path.mt0")
+	bounds = [(1,20),(1,20),(1,20)]
+	initial_value = [1, 1, 1]
 	
-	write_sweep_params("test.sp", "example", test_params)
+	minimize(Optimize_Function, initial_value, args = arguments, bounds = bounds)
--- part_2/sweep_params.sp
+++ part_2/sweep_params.sp
@@ -1,4 +1,3 @@
 .DATA sweep_params size_stage_1 size_stage_2 size_stage_3
-1	2	3
-2	3	4
+ 1.0 1.0 1.00000001
 .ENDDATA
